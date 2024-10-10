78e9309273f5fd33bbbef13f3de5000b83117bef
WQH98 <2361125846@qq.com>
2024-10-10 16:19:21 +0800

    完成CheckBox

---
+++ 01_tiange/07_CheckBox/app/src/main/java/com/example/a07_checkbox/CheckBoxActivity.java
@@ -0,0 +1,45 @@
+package com.example.a07_checkbox;
+
+import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class CheckBoxActivity extends AppCompatActivity {
+
+    private CheckBox m_cb5, m_cb6;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_check_box);
+        
+        m_cb5 = findViewById(R.id.cb_5);
+        m_cb6 = findViewById(R.id.cb_6);
+        m_cb5.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                Toast.makeText(CheckBoxActivity.this, b?"5选中":"5未选中", Toast.LENGTH_SHORT).show();
+            }
+        });
+        m_cb6.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                Toast.makeText(CheckBoxActivity.this, b?"6选中":"6未选中", Toast.LENGTH_SHORT).show();
+            }
+        });
+        
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
